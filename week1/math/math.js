1cb92bd78cd21f4fbde21313de1adae1ff512aae
krenee411 <kelci.morgan@bryanuniversity.edu>
2021-11-22 16:32:02 -0600

    125 created

---
+++ week1/math/math.js
@@ -0,0 +1,19 @@
+//addition
+export default function add(){
+
+}
+
+//subtraction
+export default function sub(){
+
+}
+
+//multiplication 
+export default function multi(){
+
+}
+
+//division
+export default function div(){
+    
+}
