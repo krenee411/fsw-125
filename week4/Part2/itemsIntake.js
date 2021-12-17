2826503734df8573c326fd48b359d45966db39c0
krenee411 <kelci.morgan@bryanuniversity.edu>
2021-12-16 19:34:12 -0600

    Week4 part2 update

--- week4/Part2/itemsIntake.js
+++ week4/Part2/itemsIntake.js
@@ -12,43 +12,7 @@ app.use(express.json())
 //routes
 app.use('/recycledItems', ItemRoute)
 
-/*
-//getting data
-app.get('/recycledItems',(req,res) => {
-    res.send(recycledItems)
-    console.log(recycledItems)
-});
 
-//get one
-app.get('/:recycledId',(req,res) => {
-     const recycledId = req.params.recycledId;
-     const oneItem = recycledItems.find(item => item._id === recycledId);
-     
-     res.send(oneItem);
-})
-
-//posting data
-app.post('/recycledItems',(req,res) =>{ 
-    const newItem = req.body;
-    newItem._id = uuidv4();
-    recycledItems.push(newItem);
-    
-    console.log(recycledItems)
-    res.send('Item was successfully added')
-    
-})
-
-
-//delete data
-app.delete('/:recycledId',(req,res) => {
-    const recycledId = req.params.recycledId;
-    const oneItem = recycledItems.findIndex(item => item._id === recycledId);
-})
-
-
-//put data
-
-*/
 
 app.listen(PORT, () => {
     console.log(`App started on port : ${PORT}`)
